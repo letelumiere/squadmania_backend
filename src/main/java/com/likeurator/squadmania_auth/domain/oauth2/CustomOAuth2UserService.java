2a97ddaadc29dcbed6ac8606475ced0a3d3c41e3
letelumiere <letelumiere@gmail.com>
2023-07-06 14:24:11 +0900

    commits.

--- src/main/java/com/likeurator/squadmania_auth/domain/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/likeurator/squadmania_auth/domain/oauth2/CustomOAuth2UserService.java
@@ -1,7 +1,14 @@
 package com.likeurator.squadmania_auth.domain.oauth2;
 
+import java.util.Base64;
 import java.util.Collections;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -10,10 +17,11 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-import com.likeurator.squadmania_auth.domain.oauth2.model.OAuthAttributes;
-import com.likeurator.squadmania_auth.domain.oauth2.model.User;
 import com.likeurator.squadmania_auth.domain.user.UserRepository;
 import com.likeurator.squadmania_auth.domain.user.model.Userinfo;
 import com.nimbusds.jose.shaded.gson.Gson;
@@ -21,12 +29,20 @@ import com.nimbusds.jose.shaded.gson.Gson;
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
     private final HttpSession httpSession;
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+    
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String kakaoClientSecret;
     
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -34,40 +50,52 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         throw new UnsupportedOperationException("Unimplemented method 'loadUser'");
     }
 
-/*
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        //DefaultOAuth2User 서비스를 통해 User 정보를 가져와야 하기 때문에 대리자 생성
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+    public String kakaoCallback(String code) {
+        RestTemplate restTemplate = new RestTemplate();
 
-        OAuth2User oAuth2User = delegate.loadUser(userRequest);
-        //네이버 로그인인지 구글로그인인지 서비스를 구분해주는 코드
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String codeVerifier = "YOUR_CODE_VERIFIER";
+        String codeChallenge = "YOUR_CODE_CHALLENGE";
 
-        //OAuth2 로그인 진행시 키가 되는 필드값 프라이머리키와 같은 값 네이버 카카오 지원 x
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-                                        .getUserInfoEndpoint().getUserNameAttributeName();
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("grant_type", "authorization_code");
+        parameters.add("client_id", kakaoClientId);
+        parameters.add("client_secret", kakaoClientSecret); // Add client_secret
+        parameters.add("redirect_uri", "http://localhost:8080/api/v1/oauth2/code/kakao");
+        parameters.add("code", code);
+        parameters.add("code_verifier", codeVerifier);
 
-        //OAuth2UserService를 통해 가져온 데이터를 담을 클래스
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        //로그인 한 유저 정보
-        User user = saveOrUpdate(attributes);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(parameters, headers);
+        String credentials = kakaoClientId + ":" + kakaoClientSecret;
+        String encodedCredentials = new String(Base64.getEncoder().encode(credentials.getBytes()));
+        headers.add("Authorization", "Basic " + encodedCredentials);
 
-        //httpSession의 유저 속성을 설정
-        httpSession.setAttribute("user", new SessionUser(user));
-        // 로그인한 유저를 리턴함
-        return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
-                    attributes.getAttributes(),
-                    attributes.getNameAttributeKey());
+        ResponseEntity<String> response = restTemplate.postForEntity("https://kauth.kakao.com/oauth/token", request, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Gson gson = new Gson();
+            Map<String, Object> responseMap = gson.fromJson(response.getBody(), Map.class);
+
+            for(Object obj : responseMap.keySet()){
+                log.info(" obj : {} ", obj + " " + responseMap.get(obj));
+            }
+
+            String accessToken = (String) responseMap.get("access_token");
+            log.info("Access Token: {}", accessToken);
+            return accessToken;
+        } else {
+            log.error("Error occurred while fetching access token: {}", response.getStatusCode());
+            return "Error";
+        }
     }
 
-    //User 저장하고 이미 있는 데이터면 Update
-    private User saveOrUpdate(OAuthAttributes attributes) {
-        User user = userRepository.findByEmail(attributes.getEmail())
-                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-                .orElse(attributes.toEntity());
-        return userRepository.save(user);    }
-*/
+    public Object kakaoCallback(){
+
+        return null;
+    }
+
+    public void register(){
+    }
+
 }
--- src/main/java/com/likeurator/squadmania_auth/domain/oauth2/OAuth2Controller.java
+++ src/main/java/com/likeurator/squadmania_auth/domain/oauth2/OAuth2Controller.java
@@ -42,62 +42,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OAuth2Controller {
 
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String kakaoClientId;
-    
-    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
-    private String kakaoClientSecret;
-
     @Autowired
     private CustomOAuth2UserService oAuth2UserService;
-
-    //로직
-    //소셜 로그인 - 회원가입 - 이메일 인증 - 로그인     
-    //1. 카카오에 로그인 -> securitychainfilter로 해결
-        //세션이 구현되어야 함.
-    //2. 카카오에서 authorization code 받아서 회원가입
-    //3. 카카오에 로그인 -> 카카오 아이디로 로그인
-    //간편가입 redirectUrl 등록을 안함 ㅋ 해야 하나?
-        //accessToken이 부여됨.
-    //회원 정보에서 provider를 구분해야 하나?
-
-
-    @ResponseBody
+    
     @GetMapping("/code/kakao")
-    public String kakaoCallback(@RequestParam String code) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        String codeVerifier = "YOUR_CODE_VERIFIER";
-        String codeChallenge = "YOUR_CODE_CHALLENGE";
-
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("grant_type", "authorization_code");
-        parameters.add("client_id", kakaoClientId);
-        parameters.add("client_secret", kakaoClientSecret); // Add client_secret
-        parameters.add("redirect_uri", "http://localhost:8080/api/v1/oauth2/code/kakao");
-        parameters.add("code", code);
-        parameters.add("code_verifier", codeVerifier);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(parameters, headers);
-        String credentials = kakaoClientId + ":" + kakaoClientSecret;
-        String encodedCredentials = new String(Base64.getEncoder().encode(credentials.getBytes()));
-        headers.add("Authorization", "Basic " + encodedCredentials);
-
-        ResponseEntity<String> response = restTemplate.postForEntity("https://kauth.kakao.com/oauth/token", request, String.class);
-        if (response.getStatusCode() == HttpStatus.OK) {
-            Gson gson = new Gson();
-
-            Map<String, Object> responseMap = gson.fromJson(response.getBody(), Map.class);
-            String accessToken = (String) responseMap.get("access_token");
-            log.info("Access Token: {}", accessToken);
-            return accessToken;
-        } else {
-            log.error("Error occurred while fetching access token: {}", response.getStatusCode());
-            return "Error";
-        }
+    public ResponseEntity<String> kakaoCallback(@RequestParam String code) {
+        return ResponseEntity.ok(oAuth2UserService.kakaoCallback(code));
     }
 }
